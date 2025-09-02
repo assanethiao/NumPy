db306b481f3b8e5268bfbeb2811eb427e098c543
Assane Thiao <132925798+assanethiao@users.noreply.github.com>
2025-09-02 23:48:38 +0200

    Create analyseDeVentes.py

---
+++ analyseDeVentes.py
@@ -0,0 +1,19 @@
+"""
+Projet : Analyse de ventes d’un magasin
+
+Objectif :
+--Simuler les ventes quotidiennes d’un magasin sur 1 mois (30 jours).
+--Calculer les totaux, les moyennes et identifier les meilleurs jours.
+--Déterminer quels produits se vendent le mieux.
+
+Tâches à réaliser :
+--Crée un tableau NumPy de dimension (30, 5) représentant les ventes journalières de 5 produits (par exemple des valeurs aléatoires entre 0 et 100).
+--Calcule :
+----le total des ventes par jour (somme sur les colonnes)
+----le total des ventes par produit (somme sur les lignes)
+--Trouve :
+----le jour avec le plus gros chiffre d’affaires
+----le produit le plus vendu sur le mois
+--Calcule la moyenne de ventes par produit sur le mois.
+--Affiche un résumé clair avec ces informations.
+"""
