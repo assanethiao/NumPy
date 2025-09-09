085cfe9999109d36ffe6c8edfd90693339f3b9c9
Assane Thiao <132925798+assanethiao@users.noreply.github.com>
2025-09-09 13:32:29 +0200

    Update analyseMeteo.py

--- analyseMeteo.py
+++ analyseMeteo.py
@@ -16,3 +16,79 @@ Tâches à réaliser :
 --Détecte s’il existe une vague de chaleur (au moins 3 jours > 30°C consécutifs).
 --Affiche un résumé des résultats.
 """
+import numpy as np
+
+joursParMois = [31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31]
+
+mois = np.array([
+    "Janvier", "Février", "Mars", "Avril", "Mai", "Juin","Juillet", "Août", "Septembre", "Octobre", "Novembre", "Décembre"
+])
+tempMins = [2, 3, 5, 8, 12, 15, 18, 18, 15, 10, 6, 3]
+tempMaxs = [7, 9, 13, 16, 20, 29, 35, 32, 24, 18, 12, 8]
+
+nombreJoursTotal=365
+temperatures = []
+
+for i in range(12):
+    nbJours = joursParMois[i]
+    tMin = tempMins[i]
+    tMax = tempMaxs[i]
+    tempMois = np.random.randint(tMin, tMax + 1, size=nbJours)
+    temperatures.extend(tempMois)
+
+tabTemp = np.array(temperatures)
+
+tempMax = np.max(tabTemp)
+jourTempMax = np.argmax(tabTemp)
+
+jourCumul = 0
+moisTempMax = 0
+jourDansMoisMax = 0
+
+for i, nbJours in enumerate(joursParMois):
+    if jourTempMax < jourCumul + nbJours:
+        moisTempMax = i + 1
+        jourDansMoisMax = jourTempMax - jourCumul + 1
+        break
+    jourCumul += nbJours
+
+tempMin = np.min(tabTemp)
+jourTempMin = np.argmin(tabTemp)
+
+jourCumul = 0
+moisTempMin = 0
+jourDansMoisMin = 0
+
+for i, nbJours in enumerate(joursParMois):
+    if jourTempMin < jourCumul + nbJours:
+        moisTempMin = i + 1
+        jourDansMoisMin = jourTempMin - jourCumul + 1
+        break
+    jourCumul += nbJours
+
+tabtempMoyenne = np.zeros(12).reshape(1, 12)
+compteurJour = 0
+for i in range(12):
+    tempDuMoisCourant = 0
+    moyenneDuMois = 0
+    for j in range(joursParMois[i]):
+        tempDuMoisCourant = tempDuMoisCourant + tabTemp[compteurJour + j]
+
+    compteurJour = compteurJour + j
+    moyenneDuMois = tempDuMoisCourant /(j+1)
+    # a revoir
+    tabtempMoyenne[0, i] = moyenneDuMois
+    print(moyenneDuMois)
+    print(tabtempMoyenne[0,1])
+
+        
+print(tabTemp)
+
+
+print("Température max :", tempMax)
+print("Jour de l'année :", jourTempMax + 1)
+print(f"Date du jour le plus chaud : {jourDansMoisMax} {mois[moisTempMax -1]} (mois {moisTempMax})")
+
+print("Température min :", tempMin)
+print("Jour de l'année :", jourTempMin + 1)
+print(f"Date du jour le plus froid : {jourDansMoisMin} {mois[moisTempMin -1]} (mois {moisTempMin})")
